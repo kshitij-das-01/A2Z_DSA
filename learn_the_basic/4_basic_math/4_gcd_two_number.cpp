5446d58e9cc1b15e4e85ec937f92030eb33384c6
Kshitij Das <kshitij.das1501@gmail.com>
2026-03-12 15:38:37 +0530

    GCD Recursion and Vector factor solution done

--- learn_the_basic/4_basic_math/4_gcd_two_number.cpp
+++ learn_the_basic/4_basic_math/4_gcd_two_number.cpp
@@ -65,7 +65,7 @@ int gcd_calculate_2(int n1, int n2)
 	return result;
 }
 
-// Euclidien Algorithm
+// Euclidean Algorithm: using iteration
 void gcd_calculate_3(int n1, int n2)
 {
 	while (n1 > 0 && n2 > 0) 
@@ -83,6 +83,66 @@ void gcd_calculate_3(int n1, int n2)
 	else 
 		cout << n1;
 }
+
+// Euclidean algorithm: using recursion
+int gcd_calculate_4(int n1, int n2)
+{
+	if (n1 == 0)
+		return n2;
+	
+	return gcd_4(n2 % n1, n1);
+}
+
+// using vector to store factor and checking the max common factor
+int gcd_calculate_5(int n1, int n2)
+{
+	std::vector<int> f1, f2;
+
+	for (int i = 1; i * i <= std::min(n1, n2); i++)
+	{
+		if (n1 % i == 0) {
+			f1.push_back(i);
+
+			if (n1 / i != i)
+				f1.push_back(n1/i);
+		}
+
+		if (n2 % i == 0) {
+			f2.push_back(i);
+
+			if (n2 / i != i)
+				f2.push_back(n2/i);
+		}
+	}
+
+	std::sort(f1.begin(), f1.end());
+	std::sort(f2.begin(), f2.end());
+
+	// for (int ls : f1)
+	// 	cout << ls << ' ';
+	// cout << '\n';	
+
+	// for (int ls : f2)
+	// 	cout << ls << ' ';
+	// cout << '\n';	
+	
+	int gcd;
+	int i = f1.size() - 1, j = f2.size() - 1;
+
+	while (i > 0 && j > 0)
+	{
+		if (f1[i] == f2[j])
+			gcd = f1[i];
+
+		if (f1[i] > f2[j])
+			i--;
+		else
+			j--;
+	}
+
+	return gcd;
+}
+
 
 int main(void)
 {
